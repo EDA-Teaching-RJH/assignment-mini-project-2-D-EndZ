6ed0e9780efc0a68a87a251fe73b025a26cc5666
D-EndZ <de12@kent.ac.uk>
2026-03-26 19:21:34 +0000

    made a base class which includes the attributes for the database

--- project.py
+++ project.py
@@ -2,3 +2,12 @@ import sys
 import csv      
 import os       
 import random  
+
+class Cars:
+    #This is the base class where most of the functions will be
+    def __init__(self, make, model, year, owner):
+        # e.g. Car("Ferrari", "F40", 1992, Ethan Hook)
+        self.make = make
+        self.model = model
+        self.year = year
+        self.owner = owner
