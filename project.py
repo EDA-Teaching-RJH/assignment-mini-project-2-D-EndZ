024d1dcc34bd700c570a3276ff68b4d7b438772d
D-EndZ <de12@kent.ac.uk>
2026-03-26 19:55:26 +0000

    adding owner class

--- project.py
+++ project.py
@@ -5,36 +5,22 @@ import random
 
 class Cars:
     #This is the base class where most of the functions will be
-    def __init__(self, make, model, year, owner):
-        # e.g. Car("Ferrari", "F40", 1992, Ethan Hook)
+    def __init__(self, make, model, year, owner_name):
+        # e.g. Car("Ferrari", "F40", 1992, Dan Efoz)
         self.make = make
         self.model = model
         self.year = year
-        self.owner = owner
+        self.owner = owner_name
+    
 
-    make_list = [
-    "Toyota",
-    "Ford",
-    "Honda",
-    "BMW",
-    "Audi",
-    "Mercedes-Benz",
-    "Tesla",
-    "Nissan",
-    "Volkswagen",
-    "Porsche"
-    "Lamborghini"
-]
-    model_list = [
-    "Corolla",
-    "Mustang GT",
-    "Civic Type R",
-    "M3 Competition",
-    "A4 Quattro",
-    "E63 AMG",
-    "Model 3 Long Range",
-    "GT-R R35",
-    "Golf GTI",
-    "911 Carrera"
-    "Aventador SV"
-]
+class Owner:
+    # adding owner class to show who owns the specific car
+    def __init__(self, name):
+        self.name = name # e.g Dan Efoz
+
+    def add_car():
+
+    
+
+
+        
