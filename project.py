57499f1314a0f729c6a74628b41db4431c96d2aa
D-EndZ <de12@kent.ac.uk>
2026-03-26 19:26:47 +0000

    added a list of the cars make and models to check that the info added will be valid

--- project.py
+++ project.py
@@ -11,3 +11,30 @@ class Cars:
         self.model = model
         self.year = year
         self.owner = owner
+
+    make_list = [
+    "Toyota",
+    "Ford",
+    "Honda",
+    "BMW",
+    "Audi",
+    "Mercedes-Benz",
+    "Tesla",
+    "Nissan",
+    "Volkswagen",
+    "Porsche"
+    "Lamborghini"
+]
+    model_list = [
+    "Corolla",
+    "Mustang GT",
+    "Civic Type R",
+    "M3 Competition",
+    "A4 Quattro",
+    "E63 AMG",
+    "Model 3 Long Range",
+    "GT-R R35",
+    "Golf GTI",
+    "911 Carrera"
+    "Aventador SV"
+]
